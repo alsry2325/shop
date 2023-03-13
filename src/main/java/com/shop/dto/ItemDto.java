b3d87a25e4dadf98360427561ec6a26cbcd2a440
그램\정민교 <alsry0@naver.com>
2023-03-13 18:18:03 +0900

    회원가입 폼 작성중

--- src/main/java/com/shop/dto/ItemDto.java
+++ src/main/java/com/shop/dto/ItemDto.java
@@ -11,13 +11,13 @@ import java.time.LocalDateTime;
 public class ItemDto {
 
 
-    private Long id;
-    private String itemNm;
-    private Integer price;
-    private String itemDetail;
-    private String sellStatCd;
-    private LocalDateTime regTime;
-    private LocalDateTime updateTime;
+    private Long id;   //상품번호
+    private String itemNm; //상품이름
+    private Integer price;// 상품가격
+    private String itemDetail;//상품 설명
+    private String sellStatCd; //상품 판매 상태
+    private LocalDateTime regTime; //등록시간
+    private LocalDateTime updateTime; //수정시간
 
 
 }
---
+++ src/main/java/com/shop/entity/Member.java
@@ -0,0 +1,49 @@
+package com.shop.entity;
+
+import com.shop.constant.Role;
+import com.shop.dto.MemberFormDto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "member")
+@Getter
+@Setter
+@ToString
+public class Member {
+
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;  //회원번호
+    private String name; //회원이름
+
+    @Column(unique = true)
+    private String email;  //회원이메일
+
+    private String password;  //비밀번호
+
+    private String address; //주소
+
+    @Enumerated(EnumType.STRING)
+    private Role role;  //역할
+
+
+    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder) {
+
+        Member member = new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        member.setAddress(memberFormDto.getAddress());
+        String password = passwordEncoder.encode(memberFormDto.getPassword());
+        member.setPassword(password);
+        member.setRole(Role.USER);
+        return member;
+
+    }
+
+}
